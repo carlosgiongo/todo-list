21f39cc8fd33aa9f75b4c990d4499a7752718726
Isabela Maria Da Campo <isabeladacampo@gmail.com>
2021-12-15 10:09:27 -0300

    Add and delete task, as well a alert in case task is null

--- App.js
+++ App.js
@@ -1,29 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { v4 as uuidv4 } from 'uuid';
 import Header from './components/Header';
+import Task from './components/Task';
+import AddTask from './components/AddTask';
 
 
 export default function App() {
-  const [task, useTask] = useState([
+  const [task, setTask] = useState([
     {"task":"HTML5","done":true, "id": "1"},
     {"task":"CSS3","done":true, "id": "2"},
     {"task":"Responsive design","done":true, "id": "3"}
   ]);
+  const addTask = (text) => {
+    if(!text){
+      Alert.alert('No task?', 'Please add a task.');
+    } else {
+      setTask(prevTasks => {
+        return [{ task: text, id: uuidv4() }, ...prevTasks]
+      });
+    }
+  };
+  const deleteTask = id => {
+    setTask(prevTasks => {
+      return prevTasks.filter(task => task.id != id)
+    });
+  };
   return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <View style={styles.list}>
-          <FlatList 
-            data={task}
-            renderItem={({ item }) => (
-              <Text>{ item.task }</Text>
-            )}
-          />
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTask addTask={addTask} />
+          <View style={styles.list}>
+            <FlatList
+              data={task}
+              renderItem={({ item }) => (
+                <Task 
+                  item={ item }
+                  deleteTask={ deleteTask }
+                />
+              )}
+            />
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -33,9 +55,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff'
   },
   content: {
-    padding: 30
+    padding: 30,
+    flex: 1
   },
   list: {
-    marginTop: 30
+    marginTop: 30,
+    flex: 1
   }
 });
